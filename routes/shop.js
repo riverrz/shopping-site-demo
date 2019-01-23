3399481e244202eea431c664a6a73135255e1588
shivam123425 <shivam123425@gmail.com>
2019-01-24 00:56:27 +0530

    Server html files

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Home");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
