35ebd71750c76ddc184b49c657b95a08effcc4b9
shivam123425 <shivam123425@gmail.com>
2019-01-24 00:23:02 +0530

    Set base url for admin routes

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Server has started");
