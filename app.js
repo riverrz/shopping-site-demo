1e75fe999702fb16f33172b12d2745b245ab8f6c
shivam123425 <shivam123425@gmail.com>
2019-01-28 12:25:55 +0530

    Cleaner path to html files, add 404.html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -7,11 +8,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/admin",adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000, () => {
