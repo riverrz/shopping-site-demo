97490b2a9b89a4dcec935e21cdb563fe450ffa0b
shivam123425 <shivam123425@gmail.com>
2019-02-01 00:21:48 +0530

    Add ejs templates and render them

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const app = express();
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
