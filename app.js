8fdef9dc97f567768945659195ef6ab2d1e5a673
shivam123425 <shivam123425@gmail.com>
2019-02-01 01:23:02 +0530

    Add controllers

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
+const errorController = require("./controllers/error");
 const app = express();
 app.set("view engine", "ejs");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
   console.log("Server has started");
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const adminData = require("./admin");
+const productsControllers = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/"
-  });
-});
+router.get("/", productsControllers.getProducts);
 
 module.exports = router;
