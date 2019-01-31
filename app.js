faba0f3606cc2c9353eca0869dc05043c112db1d
shivam123425 <shivam123425@gmail.com>
2019-01-31 15:49:43 +0530

    Render shop.pug

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+app.set("view engine", "pug");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
