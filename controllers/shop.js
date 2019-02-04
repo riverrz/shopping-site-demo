4aeb56726a42fd9519706af428de581646004da9
shivam123425 <shivam123425@gmail.com>
2019-02-04 20:58:26 +0530

    Implement deleting a product from /cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,13 +20,6 @@ exports.getProduct = (req, res, next) => {
     });
   });
 };
-exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
-};
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render("shop/index", {
@@ -58,6 +51,20 @@ exports.getCart = (req, res, next) => {
         products: cartProducts
       });
     });
+  });
+};
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
   });
 };
 exports.getOrders = (req, res, next) => {
