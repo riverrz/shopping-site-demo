49364cadf25a8618dd745bc5ba6eb3cb3a6a2e00
shivam123425 <shivam123425@gmail.com>
2019-02-07 17:25:22 +0530

    Handle cart related controllers with sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,34 +38,50 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    if (!cart) {
-      return res.send("Some error occurred while getting the cart items");
-    }
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (let product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
-        products: cartProducts
+        products
       });
-    });
-  });
+    })
+    .catch(console.log);
 };
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        //...
+      }
+      return Product.findByPk(prodId)
+        .then(product => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity }
+          });
+        })
+        .then(() => {
+          res.redirect("/cart");
+        })
+        .catch(console.log);
+    })
+    .catch(console.log);
 };
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
