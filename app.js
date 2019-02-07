a4e0ccae596639dbb522293d530a0b3194d93140
shivam123425 <shivam123425@gmail.com>
2019-02-07 16:58:28 +0530

    Create associations between products, cart and user

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -36,8 +38,13 @@ Product.belongsTo(User, {
 });
 
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
+  // .sync({force: true})
   .sync()
   .then(() => {
     return User.findByPk(1);
