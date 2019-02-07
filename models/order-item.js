2b816ec84cc21611cd2d5f2783506436bcaf4f89
shivam123425 <shivam123425@gmail.com>
2019-02-07 22:02:17 +0530

    Create order and order-item and set associations

---
+++ models/order-item.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
+
+const OrderItem = sequelize.define("orderItem", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  quantity: Sequelize.INTEGER
+});
+
+module.exports = OrderItem;
