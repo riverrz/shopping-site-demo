9582013351f0f0decc221610949069576ba5bb4d
shivam123425 <shivam123425@gmail.com>
2019-02-12 00:09:24 +0530

    Add login page and get route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 const errorController = require("./controllers/error");
 
@@ -28,6 +29,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
