42bcfa480ee1f3176eb07cad9b4a325ae50b9d91
shivam123425 <shivam123425@gmail.com>
2019-02-28 21:18:39 +0530

    Add reset password page

--- controllers/auth.js
+++ controllers/auth.js
@@ -124,3 +124,17 @@ exports.postLogout = (req, res, next) => {
     res.redirect("/");
   });
 };
+
+exports.getRest = (req, res, next) => {
+  let errorMessage = req.flash("error");
+  if (errorMessage.length > 0) {
+    errorMessage = errorMessage[0];
+  } else {
+    errorMessage = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage
+  });
+};
