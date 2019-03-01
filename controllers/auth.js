7ddb7c5a3a70818ff1dfbac145efb69ca5c0de43
shivam123425 <shivam123425@gmail.com>
2019-03-01 15:15:38 +0530

    Add validations on email, password, confirmPassword

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
 const keys = require("../keys/keys");
+const { validationResult } = require("express-validator/check");
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -81,6 +82,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg
+    });
+  }
   User.findOne({ email })
     .then(userDoc => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
 
@@ -10,7 +11,27 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email"),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and atleast 5 characters." // default error message for all validators
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    })
+  ],
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
