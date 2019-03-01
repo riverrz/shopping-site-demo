9bb7a95e43791ba91cd928f949aabb376dbd70df
shivam123425 <shivam123425@gmail.com>
2019-03-01 17:59:06 +0530

    Improve UX

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,35 +24,61 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    errorMessage
+    errorMessage,
+    oldInput: {
+      email: "",
+      password: ""
+    },
+    validationErrors: []
   });
 };
 
 exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render("auth/login", {
       path: "/login",
       pageTitle: "Login",
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email,
+        password
+      },
+      validationErrors: errors.array()
     });
   }
-  const email = req.body.email;
-  const password = req.body.password;
-
   User.findOne({ email })
     .then(user => {
       if (!user) {
-        req.flash("error", "Invalid email or password");
-        return res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password",
+          oldInput: {
+            email,
+            password
+          },
+          validationErrors: []
+        });
       }
       // validate password
       bcrypt
         .compare(password, user.password)
         .then(doMatch => {
           if (!doMatch) {
-            req.flash("error", "Invalid email or password");
-            return res.redirect("/login");
+            return res.status(422).render("auth/login", {
+              path: "/login",
+              pageTitle: "Login",
+              errorMessage: "Invalid email or password",
+              oldInput: {
+                email,
+                password
+              },
+              validationErrors: []
+            });
           }
           req.session.isLoggedIn = true;
           req.session.user = user;
