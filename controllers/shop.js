b5dae971636dad9d7468358a7582fecf684b8817
shivam123425 <shivam123425@gmail.com>
2019-03-10 19:06:20 +0530

    Add pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(prods => {
@@ -37,12 +39,27 @@ exports.getProduct = (req, res, next) => {
     });
 };
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(prods => {
       res.render("shop/index", {
         prods,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -167,16 +184,18 @@ exports.getInvoice = (req, res, next) => {
       let totalPrice = 0;
       order.products.forEach(prod => {
         totalPrice += prod.quantity * prod.product.price;
-        pdfDoc.fontSize(14).text(
-          prod.product.title +
-            "-" +
-            prod.quantity +
-            " x " +
-            "$" +
-            prod.product.price
-        );
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              "-" +
+              prod.quantity +
+              " x " +
+              "$" +
+              prod.product.price
+          );
       });
-      pdfDoc.text("----")
+      pdfDoc.text("----");
       pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
 
       pdfDoc.end();
